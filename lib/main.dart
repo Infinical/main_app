b501da913c53e45793ae52d235294d11f68356de
Infinical <inficalvin@gmail.com>
2019-04-15 15:04:17 +0300

    Add app bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,17 @@ void main(){
   runApp(SignUp());
 }
 
+class SignUp extends StatelessWidget{
+  Widget build(context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.white),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Sign Up', style: TextStyle(color: Colors.teal),),
+          centerTitle: true,
+        ),
+      ),
+    );
+  }
+}
