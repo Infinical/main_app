4b66822fb9780c1a17b64a44b745d8f10c560a6a
mj <onyangomaureen95@gmail.com>
2019-04-15 16:07:16 +0300

    place textform field properties and color modification

--- lib/pages/sigin.dart
+++ lib/pages/sigin.dart
@@ -1,41 +1,70 @@
 import 'package:flutter/material.dart';
 
-class Signin extends StatelessWidget{
-  Widget build(context){
+class Signin extends StatelessWidget {
+  Widget build(context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.white),
       home: Scaffold(
+        backgroundColor: Colors.teal,
         appBar: AppBar(
-          title: Text('Sign in',style: TextStyle(color: Colors.teal),),
+          title: Text(
+            'Sign in',
+            style: TextStyle(color: Colors.teal),
+          ),
           centerTitle: true,
         ),
         body: Container(
-          child:Form(
-          child: Column(
-            children: <Widget>[
-              username(),
-              password(),
-              submit(),
-            ],
-          ),
+          child: Form(
+            child: Column(
+              children: <Widget>[
+                username(),
+                SizedBox(
+                  height: 10.0,
+                ),
+                password(),
+                SizedBox(
+                  height: 10.0,
+                ),
+                submit(),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
-  Widget username(){
-  return TextFormField(
 
-  );
+  Widget username() {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        labelText: 'Username',
+        hintText: 'Mojo',
+      ),
+    );
   }
-  Widget password(){
-  return TextFormField(
 
-  );
+  Widget password() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        labelText: 'Password',
+        hintText: '123456',
+      ),
+    );
   }
-  Widget submit(){
-  return RaisedButton(
-    
-  );
+
+  Widget submit() {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Text(
+        'Submit',
+        style: TextStyle(color: Colors.teal, fontSize: 20.0),
+      ),
+      color: Colors.white,
+      onPressed: () {},
+    );
   }
 }
