17b359f6eb4c00ccbcdc0c717f31227684d29e8f
mj <onyangomaureen95@gmail.com>
2019-04-15 17:34:03 +0300

    center form and modify buttons shape

--- lib/pages/sigin.dart
+++ lib/pages/sigin.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:lib/mixins/validation_mixins.dart';
 
-class Signin extends StatelessWidget {
+class Signin extends StatelessWidget with ValidationMixin {
+  final formKey = GlobalKey<FormState>();
 
-final formKey = GlobalKey<FormState>();
+  String name;
+  String pass;
 
   Widget build(context) {
     return MaterialApp(
@@ -17,26 +20,28 @@ final formKey = GlobalKey<FormState>();
           ),
           centerTitle: true,
         ),
-        body: Container(
+        body: Center(
+          child: Container(
           padding: EdgeInsets.all(16.0),
           child: Form(
             key: formKey,
-            child:SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                username(),
-                SizedBox(
-                  height: 10.0,
-                ),
-                password(),
-                SizedBox(
-                  height: 10.0,
-                ),
-                submit(),
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  username(),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  password(),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  submit(),
+                ],
+              ),
             ),
           ),
-        ),
+           ),
         ),
       ),
     );
@@ -44,28 +49,44 @@ final formKey = GlobalKey<FormState>();
 
   Widget username() {
     return TextFormField(
+      style: TextStyle(color: Colors.white),
       decoration: InputDecoration(
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
         labelText: 'Username',
         hintText: 'Mojo',
       ),
+      validator: validateName,
+      onSaved: (String name) {
+        name = name;
+      },
     );
   }
 
   Widget password() {
     return TextFormField(
+      style: TextStyle(color: Colors.white),
       obscureText: true,
       decoration: InputDecoration(
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0),
+         borderSide: new BorderSide(
+            color: Colors.white,
+            width: 1.0,),
+            ),
         labelText: 'Password',
         hintText: '123456',
+       
       ),
+   
+      validator: validatePass,
+      onSaved: (String password) {
+        pass = password;
+      },
     );
   }
 
   Widget submit() {
     return RaisedButton(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Text(
         'Submit',
         style: TextStyle(color: Colors.teal, fontSize: 20.0),
