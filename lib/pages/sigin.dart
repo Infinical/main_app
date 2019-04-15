8241a833f500f7f5e22ba39f359325cea32558d7
mj <onyangomaureen95@gmail.com>
2019-04-15 17:39:41 +0300

    format document

--- lib/pages/sigin.dart
+++ lib/pages/sigin.dart
@@ -22,26 +22,26 @@ class Signin extends StatelessWidget with ValidationMixin {
         ),
         body: Center(
           child: Container(
-          padding: EdgeInsets.all(16.0),
-          child: Form(
-            key: formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  username(),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  password(),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  submit(),
-                ],
+            padding: EdgeInsets.all(16.0),
+            child: Form(
+              key: formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    username(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    password(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    submit(),
+                  ],
+                ),
               ),
             ),
           ),
-           ),
         ),
       ),
     );
@@ -67,16 +67,16 @@ class Signin extends StatelessWidget with ValidationMixin {
       style: TextStyle(color: Colors.white),
       obscureText: true,
       decoration: InputDecoration(
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0),
-         borderSide: new BorderSide(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+          borderSide: new BorderSide(
             color: Colors.white,
-            width: 1.0,),
-            ),
+            width: 1.0,
+          ),
+        ),
         labelText: 'Password',
         hintText: '123456',
-       
       ),
-   
       validator: validatePass,
       onSaved: (String password) {
         pass = password;
